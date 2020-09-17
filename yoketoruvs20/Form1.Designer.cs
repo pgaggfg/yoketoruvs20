a60f7fee46727ad197764969b95dd459f457fe7b
A601-15 <tnk.dat.class@gmail.com>
2020-09-17 11:17:03 +0900

    状態遷移実装

--- yoketoruvs20/Form1.Designer.cs
+++ yoketoruvs20/Form1.Designer.cs
@@ -111,7 +111,7 @@
             this.gameOverLabel.BackColor = System.Drawing.Color.Transparent;
             this.gameOverLabel.Font = new System.Drawing.Font("小塚ゴシック Pr6N B", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.gameOverLabel.ForeColor = System.Drawing.Color.Blue;
-            this.gameOverLabel.Location = new System.Drawing.Point(219, 167);
+            this.gameOverLabel.Location = new System.Drawing.Point(222, 183);
             this.gameOverLabel.Name = "gameOverLabel";
             this.gameOverLabel.Size = new System.Drawing.Size(363, 117);
             this.gameOverLabel.TabIndex = 6;
@@ -128,6 +128,7 @@
             this.titleButton.TabIndex = 7;
             this.titleButton.Text = "タイトルへ";
             this.titleButton.UseVisualStyleBackColor = false;
+            this.titleButton.Click += new System.EventHandler(this.titleButton_Click);
             // 
             // clearLabel
             // 
@@ -135,7 +136,7 @@
             this.clearLabel.BackColor = System.Drawing.Color.Transparent;
             this.clearLabel.Font = new System.Drawing.Font("小塚ゴシック Pr6N B", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.clearLabel.ForeColor = System.Drawing.Color.Red;
-            this.clearLabel.Location = new System.Drawing.Point(264, 167);
+            this.clearLabel.Location = new System.Drawing.Point(257, 66);
             this.clearLabel.Name = "clearLabel";
             this.clearLabel.Size = new System.Drawing.Size(273, 117);
             this.clearLabel.TabIndex = 8;
