38b7a97d297eecfd25d35c39e9a25b933fa1cf05
A601-15 <tnk.dat.class@gmail.com>
2020-09-17 11:36:11 +0900

    キャラクター定数定義

--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -15,6 +15,21 @@ namespace yoketoruvs20
     {
         const bool isDebug = true;
 
+        const int PlayerMax = 1;
+        const int EnemyMax = 10;
+        const int ItemMax = 10;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] chrs = new Label[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;
+        const int ItemIndex = EnemyIndex + EnemyMax;
+
+        const string PlayerText = "(';')";
+        const string EnemyText = "🔶";
+        const string ItemText = "💎";
+
+        static Random rand = new Random();
+
         enum State
         {
             None = -1,  // 無効
